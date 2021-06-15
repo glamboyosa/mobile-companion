7979dbac5a26c589f142c4904fd7248f9c7bc81e
glamboyosa <ogbemudiatimothy@gmail.com>
2021-06-15 10:46:14 +0100

    chore: prettier formatting

--- server/index.js
+++ server/index.js
@@ -1,59 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const { createAccessToken } = require('./helpers/createAccessToken');
-const { createPhoneCheck } = require('./helpers/createPhoneCheck');
-const { getPhoneCheck } = require('./helpers/getPhoneCheckResult');
+const express = require('express')
+const cors = require('cors')
+const { createAccessToken } = require('./helpers/createAccessToken')
+const { createPhoneCheck } = require('./helpers/createPhoneCheck')
+const { getPhoneCheck } = require('./helpers/getPhoneCheckResult')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 // create PhoneCheck
 app.post('/api/phone-check', async (req, res) => {
-  const { phone_number: phoneNumber } = req.body;
+  const { phone_number: phoneNumber } = req.body
 
   // create access token
-  const accessToken = await createAccessToken();
+  const accessToken = await createAccessToken()
 
   try {
     // create PhoneCheck resource
 
     const { checkId, checkUrl, numberSupported } = await createPhoneCheck(
       phoneNumber,
-      accessToken
-    );
+      accessToken,
+    )
 
     if (!numberSupported) {
-      res.status(400).send({ message: 'number not supported' });
+      res.status(400).send({ message: 'number not supported' })
 
-      return;
+      return
     }
-    // send push notification 
+    // send push notification
     res
       .status(201)
-      .send({ data: { checkId, checkUrl }, message: 'PhoneCheck created' });
+      .send({ data: { checkId, checkUrl }, message: 'PhoneCheck created' })
   } catch (e) {
-    res.status(400).send({ message: e.message });
+    res.status(400).send({ message: e.message })
   }
-});
+})
 
 // get PhoneCheck response
 
 app.get('/api/phone-check', async (req, res) => {
   // get the `check_id` from the query parameter
-  const { check_id: checkId } = req.query;
+  const { check_id: checkId } = req.query
   try {
     // get the PhoneCheck response
-    const { match } = await getPhoneCheck(checkId);
+    const { match } = await getPhoneCheck(checkId)
 
-    res.status(200).send({ data: { match }, message: 'successful match' });
+    res.status(200).send({ data: { match }, message: 'successful match' })
   } catch (e) {
-    res.status(400).send({ message: e.message });
+    res.status(400).send({ message: e.message })
   }
-});
+})
 
 // setup server
 app.listen(4000, () => {
-  console.log('listening on PORT 4000');
-});
+  console.log('listening on PORT 4000')
+})
