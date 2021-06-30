edb5e46a1c7ab04259b7c445ab6a0de8b9a96278
glamboyosa <ogbemudiatimothy@gmail.com>
2021-07-01 00:21:46 +0100

    feat: Polling function

---
+++ web/src/helpers/polling.js
@@ -0,0 +1,21 @@
+//https://gist.github.com/twmbx/2321921670c7e95f6fad164fbdf3170e#gistcomment-3053587
+function poll(fn, timeout, interval) {
+  const endTime = Number(new Date()) + (timeout || 2000)
+  interval = interval || 100
+
+  const checkCondition = (resolve, reject) => {
+    let ajax = fn()
+    ajax.then((response) => {
+      if (response.statusText === 'OK') {
+        resolve(response.data)
+      } else if (Number(new Date()) < endTime) {
+        setTimeout(checkCondition, interval, resolve, reject)
+      } else {
+        reject(new Error('time out for ' + fn + ' : ' + arguments))
+      }
+    })
+  }
+  return new Promise(checkCondition)
+}
+
+export { poll }
